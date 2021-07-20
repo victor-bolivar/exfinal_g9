81f3b48631770fa3f2c1798ec807aa5e20bf7191
Victor Bolivar <victor.bolivar@pucp.edu.pe>
2021-07-20 12:23:35 -0500

    pagina index.html con estructura base

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+// Servidor express
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3001
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// Socket.io
+const http = require("http");
+const socketIO = require("socket.io");
+let servidorHttp = http.Server(app);
+let socketio = socketIO(servidorHttp);
+
+// Rutas
+app.get('/', (req, res) => res.sendFile(__dirname + "/index.html"))
 app.listen(port, () => console.log(`Example app listening on port port!`))
